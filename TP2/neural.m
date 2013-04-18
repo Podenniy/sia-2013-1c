6427a9fb68985bca91529eeed4ec3d0e4c08be94
Esteban Ordano <eordano@monits.com>
2013-04-18 14:27:33 -0300

    Minor fixes to algorithms that were totally wrong

--- TP2/neural.m
+++ TP2/neural.m
@@ -1,5 +1,3 @@
-a=1;
-
 function y = rows(x)
   y = size(x)(1);
 endfunction
@@ -8,13 +6,17 @@ function y = cols(x)
   y = size(x)(2);
 endfunction
 
+function y = lvl(x)
+  y = char(x .+ toascii('@'));
+endfunction
+
 function [V, H] = run_network(W, g, E)
   O = E;
   V = struct();
   H = struct();
   levels = nfields(W);
   for level=1:levels           # For each layer of the network
-      name = '@' + level;
+      name = lvl(level);
       w = W.(name);         # using the weight matrix
       h = w * [O; -1];       # Add the threshold -1 at the end
       O = g(h);              # get the output vector
@@ -31,9 +33,9 @@ function W = get_random_w(levels, neurons_in_level, abs_cap = 0.3)
 
   W = struct();
   for level=1:levels-1
-    W.('@' + level) = cap(rand(neurons_in_level, neurons_in_level + 1));
+    W.(lvl(level)) = cap(rand(neurons_in_level, neurons_in_level + 1));
   endfor
-  W.('@' + levels-1) = cap(rand(1, neurons_in_level + 1));
+  W.(lvl(levels)) = cap(rand(1, neurons_in_level + 1));
 endfunction
 
 function y = g(x)
@@ -47,17 +49,20 @@ endfunction
 function W = backpropagation_learning(W, V, H, gp, S, eta)
 
   levels = nfields(W);
-  last_level = '@' + levels;
+  last_level = lvl(levels);
 
   # First step of the backpropagation
   last_delta = gp(H.(last_level)) .* (S-V.(last_level));
   W.(last_level) = W.(last_level) + (eta .* last_delta * V.(last_level)');
 
-  for level = last_level-1:-1:'A'
+  for level = levels-1:-1:1
 
-    current_w = W.(level);
-    last_delta = gp(H.(level)) .* current_w * last_delta;
-    W.(level) = current_w + eta .* last_delta * V.(level)';
+    name = lvl(level);
+    next = lvl(level+1);
+    next_w = W.(next);
+    next_w = next_w(:,1:cols(next_w)-1);
+    last_delta = gp(H.(name)) .* (next_w * last_delta');
+    W.(name) = W.(name) + eta .* (last_delta * V.(name));
 
   endfor
 endfunction
@@ -78,8 +83,9 @@ function W=learn(dataset, expected, levels, neurons, g, gp, eta)
       S = expected(i);
       [V, H] = run_network(W, @g, E);
       W = backpropagation_learning(W, V, H, @gp, S, eta);
+      display(W);
 
-      if abs(V.('@' + levels)-S) > 0.0001
+      if abs(V.(lvl(levels))-S) > 0.0001
         flag = 0;
       endif
 
