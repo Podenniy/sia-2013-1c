95d6c7f74dc960bcd4bd5fcf38f02279b43cf58b
Esteban Ordano <eordano@monits.com>
2013-04-20 16:15:40 -0300

    Randomize if things don't look right

--- TP2/learn.m
+++ TP2/learn.m
@@ -1,13 +1,16 @@
 function W=learn(dataset, expected, W, eta)
 
+  NO_VARIATION_ITERATIONS = 3500;
+  NO_VARIATION_DIFF = 0.01;
+  
   levels = size(fieldnames(W), 1);
   flag = 0;
-  iter = 1;
+  iter = 0;
+  last = [];
 
   P = [];
   while flag == 0
     flag = 1;
-    iter = iter + 1;
     for i=1:size(dataset, 2)
 
       E = dataset(:,i);
@@ -17,16 +20,32 @@ function W=learn(dataset, expected, W, eta)
       H = networkresult.H;
       W = backpropagation_learning(W, V, H, S, eta);
 
-      P = [P sum(V.(lvl(levels+1))-S)];
+      P = [P norm(V.(lvl(levels+1))-S)];
 
-      if abs(V.(lvl(levels+1))-S) > 0.1
+      if norm(V.(lvl(levels+1))-S) > 0.1
         flag = 0;
       end
 
     end
     
-    % this can be compared directly with "expected"
-    y = get_xor_results(W);
+    if mod(iter, NO_VARIATION_ITERATIONS) == 0
+      current = [];
+      for i=1:size(dataset, 2)
+        networkresult = run_neural_network(W, E);
+        current = [current networkresult.V.(lvl(levels+1))];
+      end
+      if ~all(last == 0) && (norm(current - last) < NO_VARIATION_DIFF)
+        display('Things didnt change in a while, randomizing');
+        matrix_dims = [size(W.(lvl(1)), 2)-1];
+        for i=1:levels
+          matrix_dims = [matrix_dims size(W.(lvl(i)), 1)];
+        end
+        W = get_random_w(matrix_dims, 0.5);
+      end
+      last = current;
+    end
+    
+    iter = iter + 1;
   end
   plot(P);
   display(['Took me ' num2str(iter) ' iterations to reduce error to <0.1'])
