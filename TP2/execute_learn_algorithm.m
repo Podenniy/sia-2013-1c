753bba545be603570c906306e7838347c12e9556
Juan Pablo Civile <elementohb@gmail.com>
2013-04-21 17:53:51 -0300

    Fix issues in test scripts

--- TP2/execute_learn_algorithm.m
+++ TP2/execute_learn_algorithm.m
@@ -5,15 +5,15 @@ function data = execute_learn_algorithm(W, matrix_topology, g, gp, window_size, 
   display(['********']);
   print_matrix(W);
   display(['********']);
-  
+
   load('TimeSerie_G1.mat');
-  x = (x + 4) ./ 8;
+  x = x ./ 4;
   dataset = zeros(window_size, 800);
   for i=1:800
     dataset(:,i) = x(i:i+window_size-1)';
   end
-  expected = x(window_size+2:801+window_size);
-  
+  expected = x(window_size+1:800+window_size);
+
   test_set = zeros(window_size, 200-window_size);
   for i=801:1000-window_size
     test_set(:,i-800) = x(i:i+window_size-1)';
@@ -29,5 +29,5 @@ function data = execute_learn_algorithm(W, matrix_topology, g, gp, window_size, 
     y = y.V.(lvl(size(fieldnames(y.V), 1)));
     testset_output(i) = y;
   end
-  
+
 end
