40815e97c8763259bfdbcc0fe1a4ee504c97ffbd
Esteban Ordano <eordano@monits.com>
2013-04-21 10:37:59 -0300

    Function to plot a neural network to dot

--- TP2/plotNeural.m
+++ TP2/plotNeural.m
@@ -3,6 +3,8 @@ function nodes = plotNeural(W)
   nodes = [];
   display('digraph{');
   
+  display(' splines = false;');
+  
   w = W.(lvl(1));
   level_names = ['Z'];
   for j=1:size(w, 2)-1
@@ -12,14 +14,17 @@ function nodes = plotNeural(W)
   
   limits = [0, size(nodes, 1)];
   
+  top_value = 0;
+  
   for level=1:size(fieldnames(W), 1)
     name = lvl(level);
     w = W.(name);
+    top_value = max(top_value, max(max(abs(w))));
     level_names = [level_names; name];
     
     for j=1:size(w, 1)
       nodes = [nodes; j, level, w(j, end)];
-      display([' ' name '_' num2str(j) ' [label="' num2str(level) '_' num2str(j) '"];']);
+      display([' ' name '_' num2str(j) ' [label="' num2str(j) '(' num2str(w(j, end)) ')"];']);
     end
     
     limits = [limits, limits(end) + size(w, 1)];
@@ -33,11 +38,17 @@ function nodes = plotNeural(W)
     for j=1:size(w, 1)
       for i=1:size(w, 2)-1
         edges = [edges; limits(level)+i, limits(level+1)+j, w(j,i)];
-        display([' ' level_names(level) '_' num2str(i) ' -> ' level_names(level+1) '_' num2str(j) ' [label="' num2str(w(j,i)) '"];']);
+        
+        if w(j,i) > 0
+          color = ['0,1,' num2str((w(j,i) / top_value))];
+        else
+          color = ['0.7,1,' num2str((w(j,i) / top_value))];
+        end
+        display([' ' level_names(level) '_' num2str(i) ' -> ' level_names(level+1) '_' num2str(j) ' [color="' color '", penwidth=' num2str(w(j,i)) '];']);
       end
     end
   end
-  display('};');
+  display('}');
   % grPlot(nodes, edges);
 
 end
