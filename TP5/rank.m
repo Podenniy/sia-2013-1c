e53496b6b30295585aa367dba50f646392c0c9c9
Esteban Ordano <eordano@monits.com>
2013-05-29 20:36:19 -0300

    Rank algorithm of selection

---
+++ TP5/rank.m
@@ -0,0 +1,49 @@
+function t=rank(individuals, M)
+
+  N = size(individuals, 2);
+  t = [];
+
+  minchances = zeros(N, 1);
+  chances = zeros(N, 1);
+  
+  ranks = zeros(N, 2);
+  for i=1:N
+    ranks(i,:) = [individuals(i).fitness, i];
+  end
+  ranks = sortrows(ranks);
+  
+  positions = zeros(N, 1);
+  for i=1:N
+    positions(ranks(i,2)) = i;
+  end
+
+  cumm = 0;
+  for i=1:N
+    cumm = cumm + positions(i);
+    chances(i) = cumm;
+  end
+  for i=1:N
+    chances(i) = chances(i) / cumm;
+  end
+  for i=2:N
+    minchances(i) = chances(i-1);
+  end
+  
+  r = rand();
+  k = 1/M;
+  selected = 1;
+  for i=1:M
+    while minchances(selected) > r || ...
+          r > chances(selected)
+      selected = selected + 1;
+      if selected == N + 1
+        selected = 1;
+      end
+    end
+    t(i).i = individuals(selected);
+    r = r + k;
+    if r > 1
+      r = r - 1;
+    end
+  end
+end
