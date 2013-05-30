31867db42ae60d08f5f71f987aa137a201946d48
Esteban Ordano <eordano@monits.com>
2013-05-29 22:16:33 -0300

    Add anular crossover function

---
+++ TP5/anular.m
@@ -0,0 +1,28 @@
+function u, v = anular(i1, i2, L)
+  
+  N = size(i1.allels, 2);
+  i = sort([randi([1,N]), randi([1,N])]);
+  
+  t1 = randi([1,N]);
+  t2 = t1+L;
+  if t2 > N
+    c = t1;
+    t1 = t2;
+    t2 = t1;
+  end
+  
+  N = size(i1.allels, 2);
+  i = randi(1:N);
+  
+  allels1 = [i1.allels[   1:t1],...
+             i2.allels[t1+1:t2],...
+             i1.allesl[t2+1:N ]);
+           
+  allels2 = [i2.allels[   1:t1],...
+             i1.allels[t1+1:t2],...
+             i2.allesl[t2+1:N ]);
+
+  u = unflatten(i1.structure, allels1);
+  v = unflatten(i1.structure, allels2);
+                
+end
