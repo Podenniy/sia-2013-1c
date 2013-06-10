cddcee61dc9848d223d989c018ce9c11e74549df
Esteban Ordano <eordano@gmail.com>
2013-06-10 15:28:39 -0300

    Hotfix

--- TP5/src/are_similar.m
+++ TP5/src/are_similar.m
@@ -2,12 +2,10 @@ function t=are_similar(p1, p2, difference)
 
   N = size(p1.p, 2);
   
-  test = N/4;
-  t1 = randi(N, test);
-  t2 = randi(N, test);
-  
-  t = 0;
-  for i=1:test
-    t = t + abs(difference_neural(p1.p(t1(i)).allels, p2.p(t2(i)).allels));
+  t=0;
+  for t1=1:N
+    for t2=1:N
+      t = t + abs(difference_neural(p1.p(t1).allels, p2.p(t2).allels));
+    end
   end
 end
